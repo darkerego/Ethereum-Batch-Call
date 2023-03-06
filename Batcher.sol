1547fedc95b5ca420db61e1cb079b090232e8ece
Darkerego <darkerego@users.noreply.github.com>
2023-03-05 21:54:47 -0500

    Update Batcher.sol

--- Batcher.sol
+++ Batcher.sol
@@ -4,11 +4,18 @@ pragma experimental ABIEncoderV2;
 import "libs/IERC20.sol";
 
 
-contract Batcher{
+
+contract Batch{
     address private admin;
     address public thisAddress;
 
+    
+
     constructor(){
+        /*
+        Set the admin address and store 
+        address(this) to save gas later.
+        */
         assembly {
             let sender := caller()
             sstore(admin.slot, sender)
@@ -17,11 +24,13 @@ contract Batcher{
     }
 
     function auth() internal view {
+        /*
+        Theoretically cheaper than using a require statement.
+        */
         if(msg.sender != admin){
             revert();
         }
     }
-    
 
     function recoverEth() external {
         /*Allows the admin to withdraw Ethereum*/
@@ -30,14 +39,17 @@ contract Batcher{
         }
 
     function recoverTokens(address tokenAddress) external {
-        auth();
         /*Allows the admin to withdraw ERC20 tokens*/
-         uint256 b = IERC20(tokenAddress).balanceOf(thisAddress);
+        auth();
+        uint256 b = IERC20(tokenAddress).balanceOf(thisAddress);
         // TODO: the balance in assembly, maybe depending on gas effeciency
         transfer(tokenAddress, msg.sender, b);
     }
 
     function bal() internal view returns (uint256) {
+        /*
+        Most gas effecient way to get the balance of this contract.
+        */
         uint256 self;
         assembly {
             self :=selfbalance()
@@ -46,6 +58,10 @@ contract Batcher{
     }
 
     function transfer(address tokenAddress, address to, uint256 amount) internal {
+        /*
+        Assembly ERC20 token transfer. This is mostly just for fun, as i don't think 
+        we are really saving much gas doing this.
+        */
         
         bytes4 _selector = 0xa9059cbb;  // bytes4(keccak('transfer(address,uint256)'))
         assembly {
@@ -83,6 +99,9 @@ contract Batcher{
 
 
    function execute(address r, uint256 v, bytes memory d) internal {
+       /*
+       Arbitrary call in yul.
+       */
     assembly {
         let success_ := call(gas(), r, v, add(d, 0x00), mload(d), 0x20, 0x0)
         let success := eq(success_, 0x1)
@@ -93,6 +112,9 @@ contract Batcher{
     }
 
     function forward(address r, uint256 v, bytes memory d) external payable {
+        /*
+        Simply forward a single call
+        */
         uint256 b = bal() - msg.value;
         execute(r,v,d);
         if(bal() < b){revert();}
@@ -101,6 +123,14 @@ contract Batcher{
     
 
     function batchCall(bytes[] calldata _calls) external payable {
+        /*
+        Gas effecient transaction batching. Takes an array of abi encoded calls.  This is 
+        intended to be a public function, so in order to be gas effecient, the only safety 
+        check that is performed is making sure that if this contract has ether in it, 
+        the balance is not lower than it was before this is called.
+       
+        TODO: this entire function in assembly too.
+        */
         uint256 b = bal() - msg.value;
         for(uint256 i = 0; i < _calls.length; i++) {
             (address r, uint256 v, bytes memory d) = abi.decode(_calls[i], (address, uint256, bytes));
